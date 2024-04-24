c1c2a39c2fc24b16a62e8dfea64abbba2e5496cf
satishp21 <satishdpanchal786@gmail.com>
2024-04-24 11:46:53 +0530

    RPC structure

---
+++ Online_store_RPC_structure/products/src/config/index.js
@@ -0,0 +1,18 @@
+const dotEnv = require("dotenv");
+
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  dotEnv.config({ path: configFile });
+} else {
+  dotEnv.config();
+}
+
+module.exports = {
+  DB_URL: process.env.MONGODB_URI,
+  MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+  PORT: 8003,
+  APP_SECRET: "forBcrypt",
+  EXCHANGE_NAME: "ONLINE_STORE",
+  CUSTOMER_SERVICE: "customer_service",
+  SHOPPING_SERVICE: "shopping_service",
+};
